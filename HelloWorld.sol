52fc473d9243e79aa34ca8e684416b71b8e0e236
ameer017 <rajiabdullahi907@gmail.com>
2024-01-17 14:46:29 +0100

    section_one_covered

---
+++ HelloWorld.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// first step is to declare the license identify --MIT, Apache
+contract HelloWorld {
+    string public myString = "hello world";
+}
